5b8c6eddeeb15bcdfcfbdd671a1963276d151fc1
Ahmad Abdullah <135378526+Ahmad1137@users.noreply.github.com>
2024-02-16 16:18:42 +0500

    Reactrouter

---
+++ src/layout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './components/Header/Header'
+import Home from './components/Home/Home'
+import Footer from './components/Footer/Footer'
+import { Outlet } from 'react-router-dom'
+function layout() {
+  return (
+   <>
+    <Header/>
+    <Outlet/>
+    <Footer/>
+   </>
+  )
+}
+
+export default layout
